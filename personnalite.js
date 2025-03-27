acbfa2502c75ce33cdeacc820f7903f3a8fb8c62
Eredeti <93719013+Eredeti@users.noreply.github.com>
2025-03-27 16:50:01 +0400

    Update personnalite.js

--- personnalite.js
+++ personnalite.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function() {
             let selected = document.querySelector(`input[name="q${i}"]:checked`);
             if (!selected) {
                 isValid = false;
-                alert(`Tu n'as pas répondu à la question ${i}.`);
+                alert(`Tu n'as pas répondu à la question !`);
                 return;
             }
             if (selected.value === "A") countA++;
